6cf8866ce20fc2676148b1493c02f266422fd0a9
shanjiaxiang <1135630810@qq.com>
2017-11-20 15:51:07 +0800

    create project
    create project

---
+++ app/src/main/java/test/com/fragmentbestpractice/News.java
@@ -0,0 +1,24 @@
+package test.com.fragmentbestpractice;
+
+/**
+ * Created by Shanjiaxiang on 2017/11/20.
+ */
+
+public class News {
+    private String title;
+    private String content;
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public String getContent() {
+
+        return content;
+    }
+    public String getTitle() {
+
+        return title;
+    }
+}
